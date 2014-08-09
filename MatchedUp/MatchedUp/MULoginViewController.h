ae1aa9d4bca0f7810a45ce68c877d0a56c5e8669
looneylu <lcarbuccia@gmail.com>
2014-08-09 15:06:45 -0400

    initial storyboard setup

---
+++ MatchedUp/MatchedUp/MULoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  MULoginViewController.h
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/9/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MULoginViewController : UIViewController
+
+@end
---
+++ MatchedUp/MatchedUp/MULoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  MULoginViewController.m
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/9/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "MULoginViewController.h"
+
+@interface MULoginViewController ()
+
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
+
+@implementation MULoginViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - IBActions
+
+- (IBAction)loginButtonPressed:(UIButton *)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
