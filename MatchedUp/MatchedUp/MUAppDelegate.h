95c48069905c4e69647ddd2511b46aaf2e154988
looneylu <lcarbuccia@gmail.com>
2014-08-09 14:58:18 -0400

    Initial Commit

---
+++ MatchedUp/MatchedUp/MUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MUAppDelegate.h
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/9/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchedUp/MatchedUp/MUSecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  MUSecondViewController.m
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/9/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "MUSecondViewController.h"
+
+@interface MUSecondViewController ()
+
+@end
+
+@implementation MUSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
