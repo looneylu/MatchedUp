32b69d50299406cb13c23129ffd6f06601ba3ff5
looneylu <lcarbuccia@gmail.com>
2014-08-12 13:06:32 -0400

    Match View Controller setup
    UI elements and hook up

---
+++ MatchedUp/MatchedUp/MatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  MatchViewController.h
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/12/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MatchViewController : UIViewController
+
+@end
---
+++ MatchedUp/MatchedUp/MatchViewController.m
@@ -0,0 +1,49 @@
+//
+//  MatchViewController.m
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/12/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "MatchViewController.h"
+
+@interface MatchViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *matchedUserImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *currentUserImageView;
+
+@end
+
+@implementation MatchViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - IBActions
+
+- (IBAction)viewChatsButtonPressed:(id)sender
+{
+
+}
+
+- (IBAction)keepSearchingButtonPressed:(id)sender
+{
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
