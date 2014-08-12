19cc87ea535238b31c2fab895f051e1b36cb64ad
looneylu <lcarbuccia@gmail.com>
2014-08-12 13:46:00 -0400

    Added ChatViewController

---
+++ MatchedUp/MatchedUp/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.h
+//  MatchedUp
+//
+//  Created by Luis Carbuccia on 8/12/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "JSMessagesViewController.h"
+
+@interface ChatViewController : JSMessagesViewController
+
+@end
