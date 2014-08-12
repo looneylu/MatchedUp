bd0313bead087475638ca8e587dbb15f31ae577b
looneylu <lcarbuccia@gmail.com>
2014-08-12 10:34:09 -0400

    managing User Profile
    sliders and switch changes

--- MatchedUp/MatchedUp/SettingsViewController.m
+++ MatchedUp/MatchedUp/SettingsViewController.m
@@ -27,6 +27,18 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.ageSlider.value = [[NSUserDefaults standardUserDefaults] integerForKey:kCCAgeMaxKey];
+    self.showMenSwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:kCCMenEnabledKey];
+    self.showWomenSwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:kCCWomenEnabledKey];
+    self.singlesOnlySwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:kCCSingleEnabledKey];
+    
+    [self.ageSlider addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.showMenSwitch addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.showWomenSwitch addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.singlesOnlySwitch addTarget:self action:@selector(valueChanged:) forControlEvents:UIControlEventValueChanged];
+    
+    self.ageLabel.text = [NSString stringWithFormat:@"%i", (int)self.ageSlider.value];
 }
 
 #pragma mark - IBActions
@@ -35,6 +47,27 @@
 }
 
 - (IBAction)editProfileButtonPressed:(id)sender {
+}
+
+#pragma mark - Helper Methods
+
+- (void) valueChanged:(id)sender
+{
+    if (sender == self.ageSlider)
+    {
+        [[NSUserDefaults standardUserDefaults] setInteger:self.ageSlider.value forKey:kCCAgeMaxKey];
+        self.ageLabel.text = [NSString stringWithFormat:@"%i", (int)self.ageSlider.value];
+    }
+    else if (sender == self.showMenSwitch)
+        [[NSUserDefaults standardUserDefaults] setBool:self.showMenSwitch.isOn forKey:kCCMenEnabledKey];
+    else if (sender == self.showWomenSwitch)
+        [[NSUserDefaults standardUserDefaults] setBool:self.showWomenSwitch.isOn forKey:kCCWomenEnabledKey];
+    else if (sender == self.singlesOnlySwitch)
+        [[NSUserDefaults standardUserDefaults] setBool:self.singlesOnlySwitch.isOn forKey:kCCSingleEnabledKey];
+
+    
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
 }
 
 /*
